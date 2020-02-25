ace6513e3fadcce00b73b213717af50e286677b8
Mark Bassett <bassmarkie@gmail.com>
2020-02-25 15:30:55 -0600

    authorized user sessions

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,7 +1,21 @@
-import React from 'react';
-const Account = () => (
-  <div>
-    <h1>Account</h1>
-  </div>
-);
-export default Account;
+import React from 'react'
+
+import { PasswordForgetForm } from '../PasswordForget'
+import PasswordChangeForm from '../PasswordChange'
+import { withAuthorization, AuthUserContext } from '../Session'
+
+const AccountPage = () => (
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
+)
+
+const condition = authUser => !!authUser
+
+export default withAuthorization(condition)(AccountPage)
